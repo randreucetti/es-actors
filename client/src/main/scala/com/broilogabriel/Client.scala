8e543447291a05c05a6f2aa5e4f4786cf97609bf
Gabriel Broilo <broilo.gabriel@gmail.com>
2016-10-26 09:38:41 +0100

    Removed Server.scala from Client project

--- client/src/main/scala/com/broilogabriel/Client.scala
+++ client/src/main/scala/com/broilogabriel/Client.scala
@@ -7,8 +7,6 @@ import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.remote.Ack
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.elasticsearch.client.transport.TransportClient
 
 import scala.annotation.tailrec
@@ -17,9 +15,6 @@ import scala.annotation.tailrec
   * Created by broilogabriel on 24/10/16.
   */
 object Client {
-
-  val mapper = new ObjectMapper()
-  mapper.registerModule(DefaultScalaModule)
 
   def mapArgs(args: Array[String]): Map[String, String] = {
     args.filter(_.startsWith("--")).map(_.split("=") match { case Array(k, v) => k.replaceFirst("--", "") -> v }).toMap
@@ -59,8 +54,8 @@ class Client(cluster: TransportClient, index: String, scrollId: String) extends 
       self ! sendWhile(cluster, index, scrollId, sender(), uuid)
 
     case some: Int =>
-//      context.stop(self)
-//      context.system.terminate()
+      //      context.stop(self)
+      //      context.system.terminate()
       println("Client done should wait for server.")
   }
 
